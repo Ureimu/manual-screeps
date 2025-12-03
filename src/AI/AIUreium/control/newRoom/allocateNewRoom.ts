0f122e11296ff4aeb1727be2ac1e9dbc5e5ac615
Ureimu <steam1090693441@163.com>
2025-12-03 12:07:03 +0800

    feat: enhance logic of path finding

--- src/AI/AIUreium/control/newRoom/allocateNewRoom.ts
+++ src/AI/AIUreium/control/newRoom/allocateNewRoom.ts
@@ -1,6 +1,7 @@
 import { startNewRoom } from "AI/AIUreium/room/newRoom/start";
-import { avoidEnemyRooms, getCostMatrix } from "frame/construct/utils/costMatrix";
+import { avoidEnemyRooms, getCostMatrix, isEnemyRoom } from "frame/construct/utils/costMatrix";
 import { logManager } from "utils/log4screeps";
+import { checkHighwayRoomName } from "utils/roomNameUtils";
 import { getMyRoom } from "utils/roomTools";
 import { getRoomControlData } from "..";
 import { getNewRoom } from "./getRoom";
@@ -44,27 +45,63 @@ export function getMyClosestRoomForClaim(goalRoomName: string): string | undefin
             logger.debug(`${myRoomName} setting.claimNewRoom is set to false`);
             return [myRoomName, 700] as [string, number];
         }
-        const pathResult = PathFinder.search(
-            Game.rooms[myRoomName].find(FIND_MY_SPAWNS)[0].pos,
-            new RoomPosition(25, 25, goalRoomName),
-            {
-                maxOps: 100000,
-                roomCallback: avoidEnemyRooms
+
+        // find route
+        let from = Game.rooms[myRoomName].find(FIND_MY_SPAWNS)[0].pos;
+        let to = new RoomPosition(25, 25, goalRoomName);
+
+        // Use `findRoute` to calculate a high-level plan for this path,
+        // prioritizing highways and owned rooms
+        let allowedRooms = { [from.roomName]: true };
+        const route = Game.map.findRoute(from.roomName, to.roomName, {
+            routeCallback(roomName) {
+                let isHighway = checkHighwayRoomName.test(roomName);
+                let isMyRoom =
+                    Game.rooms[roomName] && Game.rooms[roomName].controller && Game.rooms[roomName].controller?.my;
+                if (isHighway || isMyRoom) {
+                    return 1;
+                } else if (isEnemyRoom(roomName)) {
+                    return Infinity;
+                } else {
+                    return 2.5;
+                }
             }
-        );
+        });
+        if (route === -2) {
+            logger.debug(`${myRoomName} cannot find path route to ${goalRoomName}`);
+            return [myRoomName, 700] as [string, number];
+        } else {
+            route.forEach(function (info) {
+                allowedRooms[info.room] = true;
+            });
+        }
+
+        // find path
+        const pathResult = PathFinder.search(from, to, {
+            maxOps: 100000,
+            roomCallback: (roomName: string) => {
+                if (allowedRooms[roomName] === undefined) {
+                    return false;
+                }
+                return getCostMatrix(roomName, { swampCost: 1, plainCost: 1, wallCost: 0xff, roadCost: 1 });
+            },
+            maxCost: 580
+        });
         if (pathResult.path[pathResult.path.length - 1].roomName !== goalRoomName) {
             logger.debug(
                 `${myRoomName} cannot find path to ${goalRoomName}, last path pos: ${
                     pathResult.path[pathResult.path.length - 1]
-                }`
+                }, ops cost:${pathResult.ops}, length: ${pathResult.path.length}`
             );
             return [myRoomName, 700] as [string, number];
         } else {
-            logger.debug(`${myRoomName} find path to ${goalRoomName} with length: ${pathResult.path.length}`);
+            logger.debug(
+                `${myRoomName} find path to ${goalRoomName} with length: ${pathResult.path.length}, ops cost:${pathResult.ops}`
+            );
             return [myRoomName, pathResult.path.length] as [string, number];
         }
     })
-        .filter(a => a[1] < 500)
+        .filter(a => a[1] < 580)
         .sort((a, b) => a[1] - b[1])?.[0]?.[0];
     return closestRoom;
 }
--- src/frame/construct/utils/costMatrix.ts
+++ src/frame/construct/utils/costMatrix.ts
@@ -2,48 +2,60 @@ import { wrapFunction } from "utils/profiler/wrap/function";
 import { PosStr } from "utils/RoomPositionToStr";
 import { LayoutDataNode } from "../type";
 
-export const getCostMatrix = wrapFunction("getCostMatrix", (roomName: string): CostMatrix => {
-    // 在这个示例中，`room` 始终存在
-    // 但是由于 PathFinder 支持跨多房间检索
-    // 所以你要更加小心！
-    const costs = new PathFinder.CostMatrix();
-    const terrain = new Room.Terrain(roomName);
-    for (let y = 0; y < 50; y++) {
-        for (let x = 0; x < 50; x++) {
-            const tile = terrain.get(x, y);
-            const weight =
-                tile === TERRAIN_MASK_WALL
-                    ? 255 // 墙壁 => 无法通行
-                    : tile === TERRAIN_MASK_SWAMP
-                    ? 10 // 沼泽
-                    : 2; // 平原
-            costs.set(x, y, weight);
+export const getCostMatrix = wrapFunction(
+    "getCostMatrix",
+    (
+        roomName: string,
+        opts?: { swampCost: number; plainCost: number; wallCost: number; roadCost: number }
+    ): CostMatrix => {
+        // 在这个示例中，`room` 始终存在
+        // 但是由于 PathFinder 支持跨多房间检索
+        // 所以你要更加小心！
+        const { swampCost, plainCost, wallCost, roadCost } = opts ?? {
+            swampCost: 10,
+            plainCost: 2,
+            wallCost: 0xff,
+            roadCost: 1
+        };
+        const costs = new PathFinder.CostMatrix();
+        const terrain = new Room.Terrain(roomName);
+        for (let y = 0; y < 50; y++) {
+            for (let x = 0; x < 50; x++) {
+                const tile = terrain.get(x, y);
+                const weight =
+                    tile === TERRAIN_MASK_WALL
+                        ? wallCost // 墙壁 => 无法通行
+                        : tile === TERRAIN_MASK_SWAMP
+                        ? swampCost // 沼泽
+                        : plainCost; // 平原
+                costs.set(x, y, weight);
+            }
         }
+
+        const notWalkableBuildingExpandList: BuildableStructureConstant[] = [
+            "extension",
+            "spawn",
+            "link",
+            "constructedWall",
+            "storage",
+            "tower",
+            "observer",
+            "powerSpawn",
+            "extractor",
+            "lab",
+            "terminal",
+            "nuker",
+            "factory"
+        ];
+
+        setStructureCost(roomName, notWalkableBuildingExpandList, 0xff, costs);
+
+        const roadExpandList: BuildableStructureConstant[] = ["road"];
+        setStructureCost(roomName, roadExpandList, roadCost, costs);
+
+        return costs;
     }
-
-    const notWalkableBuildingExpandList: BuildableStructureConstant[] = [
-        "extension",
-        "spawn",
-        "link",
-        "constructedWall",
-        "storage",
-        "tower",
-        "observer",
-        "powerSpawn",
-        "extractor",
-        "lab",
-        "terminal",
-        "nuker",
-        "factory"
-    ];
-
-    setStructureCost(roomName, notWalkableBuildingExpandList, 0xff, costs);
-
-    const roadExpandList: BuildableStructureConstant[] = ["road"];
-    setStructureCost(roomName, roadExpandList, 1, costs);
-
-    return costs;
-});
+);
 
 function setStructureCost(
     roomName: string,
@@ -66,10 +78,21 @@ function setStructureCost(
     });
 }
 
-export function avoidEnemyRooms(roomName: string): boolean | CostMatrix {
+export function avoidEnemyRooms(
+    roomName: string,
+    opts?: { swampCost: number; plainCost: number; wallCost: number; roadCost: number }
+): boolean | CostMatrix {
     if (Memory.rooms[roomName]?.owner) {
         const owner = Memory.rooms[roomName].owner;
         if (owner !== global.gameUserName) return false;
     }
-    return getCostMatrix(roomName);
+    return getCostMatrix(roomName, opts);
+}
+
+export function isEnemyRoom(roomName: string): boolean {
+    if (Memory.rooms[roomName]?.owner) {
+        const owner = Memory.rooms[roomName].owner;
+        if (owner !== global.gameUserName) return true;
+    }
+    return false;
 }
